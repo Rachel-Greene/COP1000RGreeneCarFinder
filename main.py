c21531577127ea0b01e5abd3e315a064cff2e7ee
Rachel-Greene <159064114+Rachel-Greene@users.noreply.github.com>
2024-03-27 22:24:42 +0000

    WorkingProject

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
-## This is the program to run the AutoCountry Vehicle Finder app, version 0.1
-# Functions 
+## This is the program to run the AutoCountry Vehicle Finder app, version 0.1 ##
+## ------------------------------ Functions ------------------------------ ##
 def checkValidInput(testInput):
   if testInput == "1":
     return True
   elif testInput == "2":
-    print("Thank you for using AutoCountry Car Finder, good-bye!")
+    print("\nThank you for using AutoCountry Car Finder, good-bye!")
     exit()
   else:
     print(f"Sorry, {testInput} is not a valid input.\nPlease try again with the correct input from the provided list.")
     exit()
 
 
-## ----- Vehicles Array ----- ##
+## --------------------------- Declerations ---------------------------- ##
 carMakes = ["Ford F-150", "Chevrolet Silverado", "Tesla CyberTruck", "Toyota Tundra", "Nissan Titan"]
-
-## ----- Customer Messages ----- ##
 welcomeMessage = """
   ********************************
   AutoCountry Vehicle Finder v0.1
@@ -24,17 +22,17 @@ welcomeMessage = """
     1. PRINT all Authorized Vehicles
     2. Exit
 """
-choice1Message = "The AutoCountry sales manages has authorized the purcha and selling of the following vehicles:"
+choice1Message = "\nThe AutoCountry sales manages has authorized the purchase and selling of the following vehicles: "
 
-## Customer input, Validation ##
+
+## --------------------- Customer input, Validation --------------------- ##
 print(welcomeMessage)
 customerInput = input()
 trueInput = checkValidInput(customerInput)
 
-## Logic, Customer Output ##
+
+## ----------------------- Logic, Customer Output ----------------------- ##
 if trueInput == True:
-  print(choice1Message)
-  print(carMakes)
-  
-
-
+  print(f"{choice1Message}")
+  for a in range(0,5):
+    print(carMakes[a])
